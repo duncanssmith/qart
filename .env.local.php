909b04c77d58fe96b3198ce9e1424962c23f25f4
Duncan Smith <duncanssmith@gmail.com>
2015-08-04 13:03:59 +0100

    Initial commit

---
+++ .env.local.php
@@ -0,0 +1,9 @@
+ <?php
+
+return [
+    'DB_HOST'       => 'localhost',
+    'DB_NAME'       => 'dart',
+    'DB_USERNAME'   => 'dart',
+    'DB_PASSWORD'   => 'sienna',
+    'PUBLIC_BASE_PATH'   => '/home/vagrant/dart.local/public/',
+];
